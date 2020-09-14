ddae9454f11132617b4c9fa8140afee5b7f915b0
Tommalieh <muhammedtommalieh@gmail.com>
2020-09-14 12:14:06 +0300

    created and tested basic authentication

--- App.js
+++ App.js
@@ -4,13 +4,25 @@ import {NavigationContainer} from '@react-navigation/native';
 import MyDrawer from './navigation/parkin.js';
 import 'react-native-gesture-handler';
 
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+
+import parkinReducer from './store/reducers/parkin.js';
+
+const rootReducer = combineReducers({
+  parkin: parkinReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk))
+
 export default function App() {
   return (
-    // <Provider store={store}>
+     <Provider store={store}>
       <NavigationContainer>
         <MyDrawer />
       </NavigationContainer>
-    /* </Provider> */
+     </Provider>
   );
 }
 
